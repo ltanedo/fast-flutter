cd33f0350bb7434a8867eddea09f085b46ad50fb
ltanedo <lloydtan@buffalo.edu>
2024-08-18 21:38:59 -0400

    init

---
+++ lib/main.dart
@@ -0,0 +1,311 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Home Page"),
+      ),
+      body: 
+      FastListView(
+        children: [
+        Text("One"),
+        Text("Two THree")
+      ],
+      reverse: true,
+      ),
+      // Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headlineMedium,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class FastListView extends StatelessWidget {
+  FastListView({
+    super.key, 
+    required this.children, 
+    // custom
+    this.scrollDirection = Axis.vertical,
+    this.reverse = false,
+    this.controller,
+    this.primary,
+    this.physics,
+    this.shrinkWrap = false,
+    this.padding,
+  });
+
+  List<Widget> children;
+  Axis scrollDirection = Axis.vertical;
+  bool reverse = false;
+  ScrollController? controller;
+  bool? primary;
+  ScrollPhysics? physics;
+  bool shrinkWrap = false;
+  EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return 
+    Expanded(child: Center(child: 
+      // children.isNotEmpty ? 
+      children == null ? 
+      ListView(
+        children: children,
+        scrollDirection:  scrollDirection,
+        reverse:  reverse,
+        controller:   controller,
+        primary:  primary,
+        physics:  physics,
+        shrinkWrap:  shrinkWrap,
+        padding:  padding,
+      )
+      : 
+      // CircularProgressIndicator()
+      // Shimmer.fromColors(
+      //   baseColor: Colors.red,
+      //   highlightColor: Colors.yellow,
+      //   child: Text(
+      //     'Shimmer',
+      //     textAlign: TextAlign.center,
+      //     style: TextStyle(
+      //       fontSize: 40.0,
+      //       fontWeight:
+      //       FontWeight.bold,
+      //     ),
+      //   ),
+      // ),
+      Shimmer.fromColors(
+          baseColor: Colors.grey.shade300,
+          highlightColor: Colors.grey.shade100,
+          enabled: true,
+          child: const SingleChildScrollView(
+            physics: NeverScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                BannerPlaceholder(),
+                BannerPlaceholder(),
+                BannerPlaceholder(),
+                // TitlePlaceholder(width: double.infinity),
+                // SizedBox(height: 16.0),
+                // ContentPlaceholder(
+                //   lineType: ContentLineType.threeLines,
+                // ),
+                // SizedBox(height: 16.0),
+                // TitlePlaceholder(width: 200.0),
+                // SizedBox(height: 16.0),
+                // ContentPlaceholder(
+                //   lineType: ContentLineType.twoLines,
+                // ),
+                // SizedBox(height: 16.0),
+                // TitlePlaceholder(width: 200.0),
+                // SizedBox(height: 16.0),
+                // ContentPlaceholder(
+                //   lineType: ContentLineType.twoLines,
+                // ),
+              ],
+            ),
+          ))
+    ))
+    ;
+  }
+}
+
+
+class BannerPlaceholder extends StatelessWidget {
+  const BannerPlaceholder({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 200.0,
+      margin: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class TitlePlaceholder extends StatelessWidget {
+  final double width;
+
+  const TitlePlaceholder({
+    Key? key,
+    required this.width,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: width,
+            height: 12.0,
+            color: Colors.white,
+          ),
+          const SizedBox(height: 8.0),
+          Container(
+            width: width,
+            height: 12.0,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum ContentLineType {
+  twoLines,
+  threeLines,
+}
+
+class ContentPlaceholder extends StatelessWidget {
+  final ContentLineType lineType;
+
+  const ContentPlaceholder({
+    Key? key,
+    required this.lineType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 96.0,
+            height: 72.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 12.0),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 10.0,
+                  color: Colors.white,
+                  margin: const EdgeInsets.only(bottom: 8.0),
+                ),
+                if (lineType == ContentLineType.threeLines)
+                  Container(
+                    width: double.infinity,
+                    height: 10.0,
+                    color: Colors.white,
+                    margin: const EdgeInsets.only(bottom: 8.0),
+                  ),
+                Container(
+                  width: 100.0,
+                  height: 10.0,
+                  color: Colors.white,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
